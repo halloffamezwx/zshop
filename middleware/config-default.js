32d7895b0c38ec311899123959170453e13d99a7
1000copy <1000copy@gmail.com>
2018-05-10 10:33:44 +0800

    add missing packages

--- middleware/config-default.js
+++ middleware/config-default.js
@@ -3,7 +3,7 @@ var config = {
     dialect: 'mysql',
     database: 'zshop',
     username: 'root',
-    password: '',
+    password: '12345678',
     host: 'localhost',
     port: 3306,
     rootpath: '/zshop'
